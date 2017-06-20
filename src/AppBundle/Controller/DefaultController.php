27cabbd01eccfd4f093fa351da3153f0546e4e5d
Santiago Figueiras <santi.figueiras95@gmail.com>
2017-06-20 13:16:29 -0300

    Changed the response in export for utility class BinaryFileResponse and added the report.csv file to the gitignore

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,7 +12,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class DefaultController extends Controller
 {
@@ -173,17 +174,11 @@ class DefaultController extends Controller
 
         fclose($fp);
         
-        $response = new Response();
-
-        $response->headers->set('Cache-Control', 'private');
-        $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s";', $filename));
-        $response->headers->set('Content-Type', mime_content_type($filename));
-        $response->headers->set('Content-length', filesize($filename));
-        $response->sendHeaders();
-
-        $response->setContent(file_get_contents($filename));
-
-        unlink($filename);
+        $response = new BinaryFileResponse($filename);
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $filename
+        );
 
         return $response;
      }
